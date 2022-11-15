9719c8586efefabb9bd364ae25abd2acf1a05d00
Jefin-J <jefinkanamala@gmail.com>
2022-11-15 13:35:12 +0530

    Fixed some connection problems
    -changed connection function to the one I got from aws github example.
     App was exiting, this is to avoid app crashing
    -changed setCertificates to useCertificate in connection function
     I was getting handshake error. This is to fix that
     -I got it connected

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -21,11 +21,13 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     _connectMQTT();
-  
   }
-  _connectMQTT() async{
-    await  mqttConnect();
+
+  _connectMQTT() async {
+    //await mqttConnect();
+    await newAWSConnectFronExample();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -115,84 +117,180 @@ class _HomeScreenState extends State<HomeScreen> {
       ]),
     );
   }
-  Future<int> mqttConnect() async {  
-  const url = 'a25p8nrysfd25r-ats.iot.us-west-2.amazonaws.com';
-  const port = 8883;
-  const clientId = 'android123';
 
-  final client = MqttServerClient.withPort(url, clientId, port);
+  Future<int> mqttConnect() async {
+    const url = 'a25p8nrysfd25r-ats.iot.us-west-2.amazonaws.com';
+    const port = 8883;
+    const clientId = 'android123';
 
+    final client = MqttServerClient.withPort(url, clientId, port);
 
-  client.secure = true;
+    client.secure = true;
 
-  client.keepAlivePeriod = 20;
-  // Set the protocol to V3.1.1 for AWS IoT Core, if you fail to do this you will not receive a connect ack with the response code
-  client.setProtocolV311();
-  // logging if you wish
-  client.logging(on: false);
+    client.keepAlivePeriod = 20;
+    // Set the protocol to V3.1.1 for AWS IoT Core, if you fail to do this you will not receive a connect ack with the response code
+    client.setProtocolV311();
+    // logging if you wish
+    client.logging(on: false);
 
-  final context = SecurityContext.defaultContext;
-  
-  final ByteData crtData = await rootBundle.load('assets/certs/DeviceCertificate.crt');
-context.setTrustedCertificatesBytes(crtData.buffer.asUint8List());
+    final context = SecurityContext.defaultContext;
 
-final ByteData authoritiesBytes = await rootBundle.load('assets/certs/RootCA.pem');
-context.setClientAuthoritiesBytes(authoritiesBytes.buffer.asUint8List());
+    final ByteData crtData =
+        await rootBundle.load('assets/certs/DeviceCertificate.crt');
+    context.setTrustedCertificatesBytes(crtData.buffer.asUint8List());
 
-final ByteData keyBytes = await rootBundle.load('assets/certs/Private.key');
-context.usePrivateKeyBytes(keyBytes.buffer.asUint8List());
+    final ByteData authoritiesBytes =
+        await rootBundle.load('assets/certs/RootCA.pem');
+    context.setClientAuthoritiesBytes(authoritiesBytes.buffer.asUint8List());
 
+    final ByteData keyBytes = await rootBundle.load('assets/certs/Private.key');
+    context.usePrivateKeyBytes(keyBytes.buffer.asUint8List());
 
-  client.securityContext = context;
+    client.securityContext = context;
 
-  final connMess = MqttConnectMessage()
-      .withClientIdentifier('android123')
-      .startClean();
-  client.connectionMessage = connMess;
+    final connMess =
+        MqttConnectMessage().withClientIdentifier('android123').startClean();
+    client.connectionMessage = connMess;
 
-  // Connect the client
-  try {
-    print('MQTT client connecting to AWS IoT using certificates....');
-    await client.connect();
-  } on Exception catch (e) {
-    print('MQTT client exception - $e');
-    client.disconnect();
-    exit(-1);
-  }
+    // Connect the client
+    try {
+      print('MQTT client connecting to AWS IoT using certificates....');
+      await client.connect();
+    } on Exception catch (e) {
+      print('MQTT client exception - $e');
+      client.disconnect();
+      exit(-1);
+    }
 
-  if (client.connectionStatus!.state == MqttConnectionState.connected) {
-    print('MQTT client connected to AWS IoT');
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('MQTT client connected to AWS IoT');
 
-    // Publish to a topic of your choice
-    const topic = 'esp32/sub';
-    final builder = MqttClientPayloadBuilder();
-    builder.addString('Hello World');
-    // Important: AWS IoT Core can only handle QOS of 0 or 1. QOS 2 (exactlyOnce) will fail!
-    client.publishMessage(topic, MqttQos.atLeastOnce, builder.payload!);
+      // Publish to a topic of your choice
+      const topic = 'esp32/sub';
+      final builder = MqttClientPayloadBuilder();
+      builder.addString('Hello World');
+      // Important: AWS IoT Core can only handle QOS of 0 or 1. QOS 2 (exactlyOnce) will fail!
+      client.publishMessage(topic, MqttQos.atLeastOnce, builder.payload!);
 
-    // Subscribe to the same topic
-    client.subscribe(topic, MqttQos.atLeastOnce);
-    // Print incoming messages from another client on this topic
-    client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-      final recMess = c[0].payload as MqttPublishMessage;
-      final pt =
-          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      // Subscribe to the same topic
+      client.subscribe(topic, MqttQos.atLeastOnce);
+      // Print incoming messages from another client on this topic
+      client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+        final recMess = c[0].payload as MqttPublishMessage;
+        final pt =
+            MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+        print(
+            'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+        print('');
+      });
+    } else {
       print(
-          'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
-      print('');
-    });
-  } else {
-    print(
-        'ERROR MQTT client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
+          'ERROR MQTT client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
+      client.disconnect();
+    }
+
+    print('Sleeping....');
+    await MqttUtilities.asyncSleep(10);
+
+    print('Disconnecting');
     client.disconnect();
+
+    return 0;
   }
 
-  print('Sleeping....');
-  await MqttUtilities.asyncSleep(10);
+  Future<int> newAWSConnectFronExample() async {
+    // Your AWS IoT Core endpoint url
+    const url = 'a25p8nrysfd25r-ats.iot.us-west-2.amazonaws.com';
+    // AWS IoT MQTT default port
+    const port = 8883;
+    // The client id unique to your device
+    const clientId = 'android123';
 
-  print('Disconnecting');
-  client.disconnect();
+    // Create the client
+    final client = MqttServerClient.withPort(url, clientId, port);
 
-  return 0;
+    // Set secure
+    client.secure = true;
+    // Set Keep-Alive
+    client.keepAlivePeriod = 20;
+    // Set the protocol to V3.1.1 for AWS IoT Core, if you fail to do this you will receive a connect ack with the response code
+    client.setProtocolV311();
+    // logging if you wish
+    client.logging(on: true);
+
+    // Set the security context as you need, note this is the standard Dart SecurityContext class.
+    // If this is incorrect the TLS handshake will abort and a Handshake exception will be raised,
+    // no connect ack message will be received and the broker will disconnect.
+    // For AWS IoT Core, we need to set the AWS Root CA, device cert & device private key
+    // Note that for Flutter users the parameters above can be set in byte format rather than file paths
+    // final context = SecurityContext.defaultContext;
+    // context.setClientAuthorities('assets/certs/RootCA.pem');
+    // context.useCertificateChain('assets/certs/DeviceCertificate.crt');
+    // context.usePrivateKey('assets/certs/Private.key');
+    // client.securityContext = context;
+
+    final context = SecurityContext.defaultContext;
+
+    final ByteData crtData =
+        await rootBundle.load('assets/certs/DeviceCertificate.crt');
+    context.useCertificateChainBytes(crtData.buffer.asUint8List());
+
+    final ByteData authoritiesBytes =
+        await rootBundle.load('assets/certs/RootCA.pem');
+    context.setClientAuthoritiesBytes(authoritiesBytes.buffer.asUint8List());
+
+    final ByteData keyBytes = await rootBundle.load('assets/certs/Private.key');
+    context.usePrivateKeyBytes(keyBytes.buffer.asUint8List());
+
+    client.securityContext = context;
+
+    // Setup the connection Message
+    final connMess =
+        MqttConnectMessage().withClientIdentifier('android123').startClean();
+    client.connectionMessage = connMess;
+
+    // Connect the client
+    try {
+      print('MQTT client connecting to AWS IoT....');
+      await client.connect();
+    } on Exception catch (e) {
+      print('MQTT client exception - $e');
+      client.disconnect();
+    }
+
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('MQTT client connected to AWS IoT');
+
+      // Publish to a topic of your choice
+      const topic = '/esp32/sub';
+      final builder = MqttClientPayloadBuilder();
+      builder.addString('Hello World');
+      // Important: AWS IoT Core can only handle QOS of 0 or 1. QOS 2 (exactlyOnce) will fail!
+      client.publishMessage(topic, MqttQos.atLeastOnce, builder.payload!);
+
+      // Subscribe to the same topic
+      client.subscribe(topic, MqttQos.atLeastOnce);
+      // Print incoming messages from another client on this topic
+      client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+        final recMess = c[0].payload as MqttPublishMessage;
+        final pt =
+            MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+        print(
+            'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+        print('');
+      });
+    } else {
+      print(
+          'ERROR MQTT client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
+      client.disconnect();
+    }
+
+    print('Sleeping....');
+    await MqttUtilities.asyncSleep(10);
+
+    print('Disconnecting');
+    client.disconnect();
+
+    return 0;
+  }
 }
-  }
